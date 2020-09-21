66a36bd9a3ebb6e26115ba744bcca59e51711ab8
Hasin Hayder <countdraculla@gmail.com>
2020-09-22 02:14:12 +0600

    Added loader

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,23 @@
 const app = Vue.createApp({
-    created(){
-        fetch("https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-name.json")
-        .then(response=>response.json())
-        .then(data=>{this.countries=data})
-    },
-    data(){
-        return {item:"",countries:[]}
-    },
-    computed:{
-        filteredCountries(){
-            return this.countries
-            .filter((country)=>country.country.toLowerCase().startsWith(this.item.toLowerCase()))
-        }
+  created () {
+    fetch(
+      'https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-name.json'
+    )
+      .then(response => response.json())
+      .then(data => {
+        this.countries = data
+        document.getElementById("loader").style.display="none";
+      })
+  },
+  data () {
+    return { item: '', countries: [] }
+  },
+  computed: {
+    filteredCountries () {
+      return this.countries.filter(country =>
+        country.country.toLowerCase().startsWith(this.item.toLowerCase())
+      )
     }
-});
-app.mount("#app");
+  }
+})
+app.mount('#app')
