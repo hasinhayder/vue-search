e1a4306239e0ac46e8a798fceab8d7687ad68832
Hasin Hayder <countdraculla@gmail.com>
2020-09-22 00:22:21 +0600

    Final Result

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,16 @@
 const app = Vue.createApp({
-    
+    created(){
+        fetch("https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-name.json")
+        .then(response=>response.json())
+        .then(data=>{this.countries=data})
+    },
+    data(){
+        return {item:"",countries:[]}
+    },
+    computed:{
+        filteredCountries(){
+            return this.countries.filter((country)=>country.country.toLowerCase().startsWith(this.item))
+        }
+    }
 });
 app.mount("#app");
